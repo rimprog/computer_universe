110bce01573af4068b37b606c75c2d1605d1b16d
rimprog <riminprog@gmail.com>
2022-03-20 19:23:01 +0300

    Create graphic card model

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import GraphicCard
+
+
+@admin.register(GraphicCard)
+class GraphicCardAdmin(admin.ModelAdmin):
+    pass
--- storage/models.py
+++ storage/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class GraphicCard(models.Model):
+    name = models.CharField('имя', max_length=200)
+    url= models.URLField('ссылка')
+    old_price = models.FloatField('старая цена')
+    current_eur_price = models.FloatField('текущая цена EUR')
+    current_rub_price = models.FloatField('текущая цена RUB')
+
+    class Meta:
+        verbose_name = 'видеокарта'
+        verbose_name_plural = 'видеокарты'
+
+    def __str__(self):
+        return self.name
