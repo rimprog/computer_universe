8f139eb75050cb77430f80ffaa1f571fd1aa46dc
rimprog <riminprog@gmail.com>
2022-03-24 12:33:01 +0300

    Create saving parsing results into database

---
+++ storage/management/commands/parse_graphic_cards.py
@@ -0,0 +1,45 @@
+import os
+import time
+
+from django.core.management.base import BaseCommand, CommandError
+from selenium import webdriver
+
+from storage.models import GraphicCard
+from storage.utils.computeruniverse_parser import parse_graphic_cards_catalogue
+
+
+class Command(BaseCommand):
+    help = 'Parsing graphic cards names, links, prices from https://www.computeruniverse.net/'
+
+    def add_arguments(self, parser):
+        pass
+
+    def handle(self, *args, **options):
+        chromedriver_path = os.path.join(os.getcwd(), 'chromedriver')
+        driver = webdriver.Chrome(chromedriver_path)
+
+        graphic_cards = parse_graphic_cards_catalogue(driver)
+
+        time.sleep(10)
+        driver.quit()
+
+        self.stdout.write(self.style.SUCCESS('\nSuccessfully parsed graphic cards from computeruniverse graphic cards catalogue.\n\nNew graphic cards was added into database:'))
+
+        created_graphic_cards = []
+        for graphic_card in graphic_cards:
+            graphic_card, is_graphic_card_created = GraphicCard.objects.get_or_create(
+                name=graphic_card['name'],
+                url=graphic_card['url'],
+                defaults = {
+                    'old_price': graphic_card['price']['old_price'],
+                    'current_eur_price': graphic_card['price']['current_eur_price'],
+                    'current_rub_price': graphic_card['price']['current_rub_price']
+                }
+            )
+
+            if is_graphic_card_created:
+                created_graphic_cards.append(graphic_card)
+                self.stdout.write(self.style.SUCCESS(graphic_card.name))
+
+        if not created_graphic_cards:
+            self.stdout.write(self.style.WARNING('New graphic cards are not found\n'))
--- storage/models.py
+++ storage/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class GraphicCard(models.Model):
     name = models.CharField('имя', max_length=200)
-    url= models.URLField('ссылка')
+    url = models.URLField('ссылка')
     old_price = models.FloatField('старая цена')
     current_eur_price = models.FloatField('текущая цена EUR')
     current_rub_price = models.FloatField('текущая цена RUB')
